d73b992e3486e6e7ae1a32f7bb0af32dfce2cbcb
Fomegne Brady <fomegnemeudje@outlook.com>
2023-05-01 16:16:06 +0100

    feat(clafrica): Implement the main program

--- clafrica/src/lib.rs
+++ clafrica/src/lib.rs
@@ -1,12 +1,109 @@
-use clafrica_lib::text_buffer;
+use clafrica_lib::{text_buffer, utils};
+use enigo::{Enigo, Key, KeyboardControllable};
+use rdev::{self, EventType, Key as E_Key};
+use std::sync::mpsc;
+use std::thread;
+use std::{env, io};
 
-struct Cursor<'a> {
-    stack: Vec<&'a str>,
-    node: &'a text_buffer::Node,
+pub struct Config {
+    data_path: String,
+    buffer_size: usize,
 }
 
-fn run() {
-    unimplemented!();
+impl Config {
+    pub fn build(mut args: env::Args) -> Result<Self, &'static str> {
+        args.next();
+        Ok(Self {
+            data_path: args.next().ok_or("filepath required")?,
+            buffer_size: 10,
+        })
+    }
+}
+
+pub fn run(config: Config) -> Result<(), io::Error> {
+    let data = utils::load_data(&config.data_path)?;
+    let map = utils::build_map(
+        data.iter()
+            .map(|e| [e[0].as_ref(), e[1].as_ref()])
+            .collect(),
+    );
+    let mut cursor = text_buffer::Cursor::new(map, config.buffer_size);
+
+    let mut keyboard = Enigo::new();
+
+    let (tx, rx) = mpsc::channel();
+    thread::spawn(move || {
+        let mut idle = false;
+
+        rdev::listen(move |event| {
+            idle = match event.event_type {
+                EventType::KeyPress(E_Key::Escape) => true,
+                EventType::KeyRelease(E_Key::Escape) => false,
+                _ => idle,
+            };
+            if !idle {
+                tx.send(event)
+                    .unwrap_or_else(|e| eprintln!("Could not send event {:?}", e));
+            }
+        })
+        .expect("Could not listen");
+    });
+
+    for event in rx.iter() {
+        let character = event.name.and_then(|s| s.chars().next());
+        let is_ascii = character.map(|c| c.is_ascii()).unwrap_or_default();
+
+        match event.event_type {
+            EventType::KeyPress(E_Key::Backspace) => {
+                if let Some(out) = cursor.undo() {
+                    keyboard.key_down(Key::Escape);
+
+                    let i = out.chars().count();
+                    (0..i).for_each(|_| keyboard.key_click(Key::Backspace));
+
+                    if let (Some(prev_out), _) = cursor.state() {
+                        keyboard.key_sequence(&prev_out);
+                    }
+
+                    keyboard.key_up(Key::Escape);
+
+                    // Clear the remaining code
+                    while let (None, _i @ 1..) = cursor.state() {
+                        cursor.undo();
+                    }
+                }
+            }
+            EventType::KeyPress(E_Key::Unknown(_) | E_Key::ShiftLeft | E_Key::ShiftRight) => {
+                println!("[ignore] {:?}", event.event_type)
+            }
+            EventType::ButtonPress(_) | EventType::KeyPress(_) if !is_ascii => {
+                cursor.clear();
+                println!("Buffer cleared");
+            }
+            EventType::KeyPress(_) => {
+                let character = character.unwrap();
+                println!("Received: {:?}", character);
+
+                let (prev_out, prev_code_len) = cursor.state();
+                let out = cursor.hit(character);
+
+                if let Some(out) = out {
+                    keyboard.key_down(Key::Escape);
+
+                    let i = prev_out.map(|s| s.chars().count()).unwrap_or(prev_code_len) + 1;
+                    (0..i).for_each(|_| keyboard.key_click(Key::Backspace));
+                    keyboard.key_sequence(&out);
+
+                    keyboard.key_up(Key::Escape);
+                };
+
+                println!("{:?}", cursor.to_path());
+            }
+            _ => (),
+        };
+    }
+
+    Ok(())
 }
 
 #[cfg(test)]
--- clafrica/src/main.rs
+++ clafrica/src/main.rs
@@ -1,3 +1,14 @@
+use clafrica::{run, Config};
+use std::{env, process};
+
 fn main() {
-    println!("Hello, world!");
+    let conf = Config::build(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    if let Err(e) = run(conf) {
+        eprintln!("Application error: {e}");
+        process::exit(1);
+    }
 }
