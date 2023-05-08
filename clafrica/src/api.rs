1792c964660c51e4eee9e5358e93c8bacbd29d5c
Fomegne Brady <fomegnemeudje@outlook.com>
2023-05-08 23:59:27 +0100

    test(clafrica): update test

--- clafrica/src/api.rs
+++ clafrica/src/api.rs
@@ -17,5 +17,6 @@ fn test_console() {
     let mut console = Console;
     console.update_screen((0, 0));
     console.update_position((0.0, 0.0));
+    Frontend::update_text(&mut console, vec!['h', 'e', 'l', 'l', 'o']);
     console.update_text(vec!['h', 'e', 'l', 'l', 'o']);
 }
