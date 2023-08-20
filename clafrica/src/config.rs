3ab813dbbe1ef47db05a971986ccf54145ecf752
Brady Fomegne <fomegnemeudje@outlook.com>
2023-08-20 15:01:19 +0100

    feat(clafrica): implement extension via scripting
    Now, the Rhai scripting language is supported as embedded scripting language.

--- clafrica/src/config.rs
+++ clafrica/src/config.rs
@@ -1,3 +1,4 @@
+use rhai::{Engine, AST};
 use serde::Deserialize;
 use std::{collections::HashMap, error, fs, path::Path};
 use toml::{self};
@@ -6,6 +7,7 @@ use toml::{self};
 pub struct Config {
     pub core: Option<CoreConfig>,
     data: Option<HashMap<String, Data>>,
+    translators: Option<HashMap<String, Data>>,
     translation: Option<HashMap<String, Data>>,
 }
 
@@ -70,6 +72,28 @@ impl Config {
             },
         )?;
         config.data = Some(data);
+
+        // Translators
+        let mut translators = HashMap::new();
+
+        config.translators.unwrap_or_default().iter().try_for_each(
+            |(key, value)| -> Result<(), Box<dyn error::Error>> {
+                match value {
+                    Data::File(DataFile { path }) => {
+                        let filepath = config_path.join(path);
+                        let conf = Config::from_file(&filepath)?;
+                        translators.extend(conf.translators.unwrap_or_default());
+                    }
+                    Data::Simple(v) => {
+                        let filepath = config_path.join(v.clone()).to_str().unwrap().to_string();
+                        translators.insert(key.to_owned(), Data::Simple(filepath));
+                    }
+                    _ => (),
+                };
+                Ok(())
+            },
+        )?;
+        config.translators = Some(translators);
 
         // Translation
         let mut translation = HashMap::new();
@@ -136,6 +160,38 @@ impl Config {
         }
     }
 
+    pub fn extract_translators(&self) -> Result<HashMap<String, AST>, Box<dyn error::Error>> {
+        let empty = HashMap::default();
+        let mut engine = Engine::new();
+
+        // allow nesting up to 50 layers of expressions/statements
+        // at global level, but only 10 inside function
+        engine.set_max_expr_depths(25, 25);
+
+        self.translators
+            .as_ref()
+            .unwrap_or(&empty)
+            .iter()
+            .filter_map(|(name, filename)| {
+                let filename = match filename {
+                    Data::Simple(filename) => Some(filename),
+                    _ => None,
+                };
+
+                filename.map(|filename| {
+                    let parent = Path::new(&filename).parent().unwrap().to_str().unwrap();
+                    let header = format!(r#"const DIR = {parent:?};"#);
+                    let ast = engine.compile_file(filename.into()).map_err(|err| {
+                        format!("Failed to parse script file `{filename}`.\nCaused by:\n\t{err}.")
+                    })?;
+                    let ast = engine.compile(header).unwrap().merge(&ast);
+
+                    Ok((name.to_owned(), ast))
+                })
+            })
+            .collect()
+    }
+
     pub fn extract_translation(&self) -> HashMap<String, String> {
         let empty = HashMap::new();
 
@@ -183,6 +239,29 @@ mod tests {
         let conf = Config::from_file(Path::new("./data/blank_sample.toml")).unwrap();
         let data = conf.extract_data();
         assert_eq!(data.keys().len(), 0);
+    }
+
+    #[test]
+    fn from_file_with_translators() {
+        use crate::config::Config;
+        use std::path::Path;
+
+        let conf = Config::from_file(Path::new("./data/config_sample.toml")).unwrap();
+        let translators = conf.extract_translators().unwrap();
+        assert_eq!(translators.keys().len(), 2);
+
+        // translators not provided
+        let conf = Config::from_file(Path::new("./data/blank_sample.toml")).unwrap();
+        let translators = conf.extract_translators().unwrap();
+        assert_eq!(translators.keys().len(), 0);
+
+        // scripts parsing error
+        let conf = Config::from_file(Path::new("./data/bad_script2.toml")).unwrap();
+        assert!(conf.extract_translators().is_err());
+
+        // script file not found
+        let conf = Config::from_file(Path::new("./data/bad_script.toml")).unwrap();
+        assert!(conf.extract_translators().is_err());
     }
 
     #[test]
